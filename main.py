42fa3c6f0885ecbdd1b3203fe01c7622211bd47e
Tez <tlatocateculti@gmail.com>
2023-08-29 17:10:02 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#Program ma za za danie gromadzić informacje o pojazdach
+
+#Musi zawierać:
+# - element przechowujący pojazdy
+# - pojazdy powinny być podzielone na typ: jednośladowe, osobowe, dostawcze, ciężarowe
+# - każdy pojazd ma posiadać informacje takie jak: numer pojazdu (tablica), kolor, przebieg
+# - użytkownik programu powinien mieć możliwość co najmniej dodawania przeglądania oraz usuwania pojazdu
+# - program powinien działać do momentu, kiedy użytkownik nie wybierze odpowiedniej opcji w menu
